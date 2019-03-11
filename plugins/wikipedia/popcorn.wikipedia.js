26a83e940b6c378abae31af305f5e64e49dc4d36
Matt Price <matt.price@utoronto.ca>
2019-03-11 14:34:30 -0400

    use supported `prop` parameter instead of deprecated `props`
    fixes #56

--- plugins/wikipedia/popcorn.wikipedia.js
+++ plugins/wikipedia/popcorn.wikipedia.js
@@ -128,7 +128,7 @@ var wikiCallback;
       };
 
       if ( options.src ) {
-        Popcorn.getScript( "//" + options.lang + ".wikipedia.org/w/api.php?action=parse&props=text&redirects&page=" +
+        Popcorn.getScript( "//" + options.lang + ".wikipedia.org/w/api.php?action=parse&prop=text&redirects&page=" +
           options.src.slice( options.src.lastIndexOf( "/" ) + 1 )  + "&format=json&callback=wikiCallback" + _guid );
       }
 
