be82561b387cf3c830c5bceb56003af0067d7ea6
Matt Price <matt.price@utoronto.ca>
2019-03-11 10:44:50 -0400

    begin refactoring Google Maps Plugin for API key

--- plugins/googlemap/popcorn.googlemap.js
+++ plugins/googlemap/popcorn.googlemap.js
@@ -27,7 +27,7 @@ var googleCallback;
     // for some reason the Google Map API adds content to the body
     if ( document.body ) {
       _mapFired = true;
-      Popcorn.getScript( "//maps.google.com/maps/api/js&key="+$apiKey+"&callback=googleCallback" );
+      Popcorn.getScript( "https://maps.googleapis.com/maps/api/js?key="+apiKey+"&callback=googleCallback");
     } else {
       setTimeout(function () {
         loadMaps();
